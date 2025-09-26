742d2fa2d3ddf8ad89b6b8547b83a4c5953368c6
Junaid2 <junaidmalik0971@gmail.com>
2025-09-26 07:30:41 -0700

    Loops and Strings

--- 4. Loops.js
+++ 4. Loops.js
@@ -26,3 +26,43 @@ while (num1 <= 5) {
 }
 
 // do while loop
+// this loop will run for atleast 1
+let i = 20;
+do {
+    console.log("This loop runs atleast once");
+}
+while (i<=10);
+
+// for-of loop (special loop)
+// helps in implementing loop on strings and arrays 
+let str = "Junaid";
+
+let size = 0;
+for (let j of str) {           //iterates thru string str (iterator)
+    console.log("j=", j)      // prints all characters of str
+    size++;
+}
+console.log("string size = ", size);
+ 
+// for-in loop
+// can be used for objects and arrays
+let student = {                             //object
+    name: "Junaid",
+    age: 20,
+    cgpa: 7.5,
+    isPass: true
+};
+
+for (let key in student) {     
+    console.log("key=", key, "value=", student[key]);         
+}
+
+
+// practice question
+let gameNum = 25;
+let userNum = prompt("Guess the game number: ");
+
+while (userNum != gameNum) {
+    userNum = prompt("You entered wrong number. Guess again : ");
+}
+console.log("congratulations, you entered the right number");
---
+++ 5. Strings.js
@@ -0,0 +1,7 @@
+// Strings
+// Sequence of characters used to represent text
+
+let str = "Junaid";
+
+// strings have some in-built properties / functions called methods
+console.log("String Length = ", str.length);
